a75e2eb6a11c1c031cfad36280def19d09cad865
Lee.Bannard <Lee Bannard@Ambrit.com>
2018-03-20 12:40:51 +0000

    Update Rustler to 0.16

--- mix.exs
+++ mix.exs
@@ -32,12 +32,10 @@ defmodule Rox.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:rustler, "~> 0.10"},
+      {:rustler, "~> 0.16"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:benchfella, "~> 0.3", only: :dev},
       {:faker, "~> 0.7", only: :dev},
-      {:flow, "~> 0.11", only: :dev},
-      {:gen_stage, "~> 0.11", only: :dev},
       {:dialyxir, "~> 0.5", only: :dev},
     ]
   end
